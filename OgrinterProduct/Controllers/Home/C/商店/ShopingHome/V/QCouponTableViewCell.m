36d592b150d398916f972b73d0bf05667f7baa96
xy605970295 <15914116027@163.com>
2019-12-16 18:21:47 +0800

    任
    16号18：25

---
+++ OgrinterProduct/Controllers/Home/C/商店/ShopingHome/V/QCouponTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  QCouponTableViewCell.m
+//  OgrinterProduct
+//
+//  Created by wongshine on 2019/12/16.
+//  Copyright © 2019 RXF. All rights reserved.
+//
+
+#import "QCouponTableViewCell.h"
+
+@implementation QCouponTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ OgrinterProduct/Controllers/Home/C/商店/ShopingHome/V/QCouponView.h
@@ -0,0 +1,17 @@
+//
+//  QCouponView.h
+//  OgrinterProduct
+//
+//  Created by wongshine on 2019/12/16.
+//  Copyright © 2019 RXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QCouponView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
