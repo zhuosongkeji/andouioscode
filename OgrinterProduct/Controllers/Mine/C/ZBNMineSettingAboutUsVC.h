0c15d1e696c77125b84b00e2ba8d0caae31ea532
Zhoufangyuan <648707352@qq.com>
2019-12-28 15:05:30 +0800

    zhouBUG修改19.28.2

--- OgrinterProduct/Controllers/Mine/C/ZBNMineSettingAboutUsVC.h
+++ OgrinterProduct/Controllers/Mine/C/ZBNMineSettingAboutUsVC.h
@@ -4,16 +4,14 @@
 //
 //  Created by 周芳圆 on 2019/12/28.
 //  Copyright © 2019 RXF. All rights reserved.
-//
+//  关于我们
 
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
-@class ZBNMineAboutUsModel;
+
 @interface ZBNMineSettingAboutUsVC : UIViewController
 
-/*! 模型 */
-@property (nonatomic, strong) ZBNMineAboutUsModel *aboutUsModel;
 @end
 
 NS_ASSUME_NONNULL_END
