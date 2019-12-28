1cc4f7262fe9c3eb7ab00d859c2542bb54b8a80e
xy605970295 <15914116027@163.com>
2019-12-28 16:49:09 +0800

    任
    12-28.3

---
+++ OgrinterProduct/Controllers/Home/C/商店/ShopingHome/M/CommentModel.h
@@ -0,0 +1,31 @@
+//
+//  CommentModel.h
+//  OgrinterProduct
+//
+//  Created by wongshine on 2019/12/28.
+//  Copyright © 2019 RXF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CommentModel : NSObject
+
+@property (nonatomic,strong) NSString *avator;
+@property (nonatomic,strong) NSString *name;
+@property (nonatomic,strong) NSString *uid;
+
+@property (nonatomic,strong) NSString *stars;
+@property (nonatomic,strong) NSString *content;
+@property (nonatomic,strong) NSString *created_at;
+
+
+-(instancetype)initWithDict:(NSDictionary *)dict;
+
++(instancetype)initWithDict:(NSDictionary *)dict;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
